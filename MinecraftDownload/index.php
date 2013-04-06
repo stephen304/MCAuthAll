474eda8fa8ce4aeae0107466417d6262ba2d3bc5
Stephen Smith <under_score_steve@hotmail.com>
2013-04-06 16:24:12 -0400

    Removed extraneous comments

--- MinecraftDownload/index.php
+++ MinecraftDownload/index.php
@@ -9,24 +9,11 @@ else {
 	$file = substr($file, 0, strpos($file, "?"));
 }
 if (file_exists($file)) {
-	//header("Last-Modified: " . date("D, d M Y ", filemtime($file)));
+	//Set the ETag that MC uses to verify the download
 	header("ETag: \"" . md5_file($file) . "\"");
-	//header("Accept-Ranges: bytes");
 	header("Content-Type: application/octet-stream");
+	//Set content length so the progress bar shows correctly
 	header("Content-Length: " . filesize($file), true);
-	//header("Connection: keep-alive");
-	//header("Server: AmazonS3");
-	
-	//header('Content-Description: File Transfer');
-	//header('Content-Type: application/octet-stream');
-	//header('Content-Disposition: attachment; filename='.basename($file));
-	//header('Content-Transfer-Encoding: binary');
-	//header('Expires: 0');
-	//header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-	//header('Pragma: public');
-	//header('Content-Length: ' . filesize($file));
-	//ob_clean();
-	//flush();
 	readfile($file);
 }
 ?>
